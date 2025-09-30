a3f0b1580d75a0e1c010db9a7d8dec5018de3951
himanshukirodiwal21 <himanshukirodiwal21@gmail.com>
2025-09-30 17:02:28 +0530

    added logo

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import UniPlayLogo from "../assets/UniPlay.svg";
 
 function Header() {
   return (
     <header>
       <nav className="container">
-        <Link to="/" className="logo">UniPlay</Link>
+        <Link to="/" className="logo">
+          <img src={UniPlayLogo} alt="UniPlay Logo" className="logo-img" />
+          <span className="logo-text">UniPlay</span>
+        </Link>
 
         <ul className="nav-links">
-          <li><Link to="/">Events</Link></li>
-          <li><Link to="/">Leaderboards</Link></li>
-          <li><Link to="/">News</Link></li>
-          <li><Link to="/about">About Us</Link></li>
+          <li>
+            <Link to="/">Events</Link>
+          </li>
+          <li>
+            <Link to="/">Leaderboards</Link>
+          </li>
+          <li>
+            <Link to="/">News</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
         </ul>
 
         <div className="auth-buttons">
           {/* Pass state to indicate which form to show */}
-          <Link to="/login" state={{ form: "login" }} className="btn btn-primary">
+          <Link
+            to="/login"
+            state={{ form: "login" }}
+            className="btn btn-primary"
+          >
             Login
           </Link>
-          <Link to="/login" state={{ form: "signup" }} className="btn btn-secondary">
+          <Link
+            to="/login"
+            state={{ form: "signup" }}
+            className="btn btn-secondary"
+          >
             Sign Up
           </Link>
         </div>
