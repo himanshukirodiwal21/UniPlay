897d5e24d26463e2ebd4d8aa5d5a6a741338e6f2
Himanshu Kirodiwal <himanshukirodiwal21@gmail.com>
2025-12-18 17:34:07 +0530

    Fix auth flow completely

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,8 +1,12 @@
+// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, // backend URL
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
